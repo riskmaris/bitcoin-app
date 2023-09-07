b153b5572df5fbc9374b34dd1d8e9893202bf75c
riskmaris <mariskaps20@gmail.com>
2023-09-07 12:30:18 +0300

    make changes on coin.js file

--- src/components/__test__/Coin.test.js
+++ src/components/__test__/Coin.test.js
@@ -1,11 +1,11 @@
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import Coin from '../Coin/Coin';
+import Coin from "../Coin/Coin";
 import store from '../../redux/store';
 
-describe ('', () => {
-  test ('Should Render Coin Component', () => {
+describe('', () => {
+  test('Should Render Coin Component', () => {
     const mockCoin = {
       uuid: 111, symbol: '', name: 'BTC', iconUrl: '', marketCap: 100, price: 1000,
     };
