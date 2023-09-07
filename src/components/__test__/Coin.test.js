31b02d1d8a60f24a5127ac5e88962f53c640c580
riskmaris <mariskaps20@gmail.com>
2023-09-07 12:24:32 +0300

    update home.js

--- src/components/__test__/Coin.test.js
+++ src/components/__test__/Coin.test.js
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import Coin from '../Coin/Coin';
 import store from '../../redux/store';
 
-describe('', () => {
-  test('Should Render Coin Component', () => {
+describe ('', () => {
+  test ('Should Render Coin Component', () => {
     const mockCoin = {
       uuid: 111, symbol: '', name: 'BTC', iconUrl: '', marketCap: 100, price: 1000,
     };
