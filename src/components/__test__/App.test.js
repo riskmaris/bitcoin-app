f94fa2ac19c30f0e4fb16a094eaf7d30c6fa301b
riskmaris <mariskaps20@gmail.com>
2023-09-08 12:27:53 +0300

    fix linter error

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../../App';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from '../../redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import App from '../../App';
+import store from '../../redux/store';
 
 test('renders learn react link', () => {
-  const {asFragment} = render( <Provider store={store}>
-     <BrowserRouter>
+  const { asFragment } = render(
+    <Provider store={store}>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
-  </Provider>);
+    </Provider>,
+  );
   expect(asFragment()).toMatchSnapshot();
 });
